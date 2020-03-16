49e2668827a4505cd233d83d06b438ae584fe664
Santosh Kumar <santoshkumar964887@gmail.com>
2020-03-16 12:39:59 +0530

    Redirect page after user login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom';
+import {Switch,Route,Redirect} from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/MainHomePage';
 import ShopPage from './Componenets/ShopPage/shopPage';
@@ -44,17 +44,20 @@ class App extends React.Component {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SinginSingup}/>
+        <Route exact path='/signin' render={()=> this.props.currentUser ?(<Redirect to='/'/>):(<SinginSingup/>)}/>
         </Switch>
       
     </div>
   );
   }
 }
+const mapStateToProps=state=>({
+  currentUser: state.user.currentUser
+});
 const mapDispatchToProps= dispatch=>({
 
  setCurrentUser: user => dispatch(setCurrentUser(user))
 
-})
+});
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
