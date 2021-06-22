312cae3316c4de130a2c54cd9fe9c7a392796650
Santosh Kumar <santosh@pickrr.com>
2021-06-22 07:32:34 +0530

    Done :homepage

---
+++ src/page/Homepage/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Dectory from "../Componenets/Directory/Dectory";
+import "./MainHomePage.Style.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Dectory />
+  </div>
+);
+export default HomePage;
