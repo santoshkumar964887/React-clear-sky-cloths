e85a4d95bacb62f8ecf88ff6696b03e8e688ddea
Santosh Kumar <santosh@pickrr.com>
2021-06-22 10:19:55 +0530

    Done:header ui

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import HomePage from "./page/Homepage";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./page/Shop";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
