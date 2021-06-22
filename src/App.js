e2d8b31312624150269231f5649358ddf46ca68e
Santosh Kumar <santosh@pickrr.com>
2021-06-22 10:05:49 +0530

    Done:shop page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import HomePage from "./page/Homepage";
 import { Switch, Route } from "react-router-dom";
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from "./page/Shop";
+import "./App.css";
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/CollectionItem/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./style.scss";
+
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className="collection-item">
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}0</span>
+    </div>
+  </div>
+);
+
+export default CollectionItem;
