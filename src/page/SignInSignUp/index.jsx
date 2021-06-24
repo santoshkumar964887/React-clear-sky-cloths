a4ad74e8670fa824a1e2d89b49b95599123b8458
Santosh Kumar <santosh@pickrr.com>
2021-06-24 10:23:35 +0530

    done: included ability to store authenticated

--- src/page/SignInSignUp/index.jsx
+++ src/page/SignInSignUp/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 import SignIn from "../../components/SignIn";
-
+import SignUp from "../../components/SignUp";
 import "./style.scss";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
